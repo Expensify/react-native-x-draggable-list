a7d57ef145fba06ee3bfaa920a8b69f78e2291a6
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2023-08-23 15:02:25 +0200

    feat: better styling

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,14 +8,21 @@ import {
   StatusBar,
   TouchableOpacity,
   Text,
+  View,
 } from 'react-native';
 import { MyDraggableList, MyScaleDecorator } from 'react-native-awesome-module';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-const COLORS = ['#FAF1E4', '#CEDEBD', '#9EB384'];
+const COLORS = {
+  LIST: ['#FAF1E4', '#CEDEBD', '#9EB384'],
+  DARK: '#435334',
+  SUBTITLE: '#49573c',
+  ACTIVE: '#d6dbd0',
+  LIGHT: '#FAF1E4',
+};
 
 const getBackgroundColor = (index: number) => {
-  return COLORS[index % COLORS.length];
+  return COLORS.LIST[index % COLORS.LIST.length];
 };
 
 const getItems = (count: number) =>
@@ -32,28 +39,30 @@ export default function App() {
   const [items, setItems] = useState(getItems(15));
 
   const renderItem = ({ item, drag, isActive }: any) => {
+    const backgroundColor = isActive ? COLORS.ACTIVE : item.content.color;
     return (
       <MyScaleDecorator>
-        <TouchableOpacity
-          onLongPress={drag}
-          disabled={isActive}
-          style={[
-            styles.rowItem,
-            {
-              backgroundColor: isActive ? '#435334' : item.content.color,
-            },
-          ]}
-        >
-          <Text style={styles.text}>{item.content.label}</Text>
-          <Text style={styles.subtitle}>{item.content.subtitle}</Text>
+        <TouchableOpacity onLongPress={drag} disabled={isActive}>
+          <View
+            style={[
+              styles.rowItem,
+              {
+                backgroundColor,
+              },
+            ]}
+          >
+            <Text style={styles.text}>{item.content.label}</Text>
+            <Text style={styles.subtitle}>{item.content.subtitle}</Text>
+          </View>
         </TouchableOpacity>
       </MyScaleDecorator>
     );
   };
 
   return (
-    <GestureHandlerRootView style={[styles.container]}>
+    <GestureHandlerRootView style={[styles.container, styles.backgroundDark]}>
       <SafeAreaView style={[styles.container, styles.androidSafeArea]}>
+        <Text style={styles.header}>Draggable demo</Text>
         <MyDraggableList
           items={items}
           renderItem={renderItem}
@@ -74,22 +83,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  backgroundDark: {
+    backgroundColor: COLORS.DARK,
+  },
   androidSafeArea: {
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
   rowItem: {
     height: 100,
-    width: 150,
+    width: 'auto',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'red',
   },
   text: {
-    color: '#435334',
+    color: COLORS.DARK,
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
   },
   subtitle: {
-    color: '#49573c',
+    color: COLORS.SUBTITLE,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingBottom: 20,
+    paddingTop: 20,
+    backgroundColor: COLORS.DARK,
+    color: COLORS.LIGHT,
   },
 });
